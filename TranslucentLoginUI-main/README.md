eedfb8f37bfdc9d6a419c86b654e6d89569305bf
bilalidrees <bilalidreesbilal2@gmail.com>
2022-01-06 16:08:53 +0500

    release commit

---
+++ TranslucentLoginUI-main/README.md
@@ -0,0 +1,3 @@
+# TranslucentLoginUI
+
+![seventh](https://user-images.githubusercontent.com/58117224/128612442-0b73a29a-3c33-4db6-91df-13c816e75cbc.jpg)
---
+++ android/app/src/main/java/com/example/translucent_login_ui/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.translucent_login_ui;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
